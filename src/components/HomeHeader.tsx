e3e01d9a8b14df4046ddf41dfd7a033841669862
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-31 13:31:13 -0300

    estilizando o HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,11 +1,16 @@
 import { Heading, HStack, VStack, Text } from "native-base";
+
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <VStack>
-        <Text color="gray.100">Olá, </Text>
+        <Text color="gray.100" fontSize="md">
+          Olá,{" "}
+        </Text>
 
-        <Heading color="gray.100">Julia</Heading>
+        <Heading color="gray.100" fontSize="md">
+          Julia
+        </Heading>
       </VStack>
     </HStack>
   );
