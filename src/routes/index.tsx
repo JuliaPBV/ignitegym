c3e74ec8ebb8736b4b8f13a2a4647712fc9db412
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-31 10:13:06 -0300

    utilizando o bottom tab

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,11 @@
 import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
+import { DefaultTheme } from "@react-navigation/native";
+import { useTheme } from "native-base";
 
 export function Routes() {
-  return <AuthRoutes />;
+  const { colors } = useTheme();
+  const theme = DefaultTheme;
+  theme.colors.background = colors.gray[700];
+  return <AppRoutes />;
 }
