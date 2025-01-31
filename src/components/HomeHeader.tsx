d246d716addc4065f26d1540e1ad17ce70986f44
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-31 13:28:07 -0300

    posicionamento na vertical e horizontal

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,12 @@
+import { Heading, HStack, VStack, Text } from "native-base";
+export function HomeHeader() {
+  return (
+    <HStack>
+      <VStack>
+        <Text color="gray.100">Olá, </Text>
+
+        <Heading color="gray.100">Julia</Heading>
+      </VStack>
+    </HStack>
+  );
+}
