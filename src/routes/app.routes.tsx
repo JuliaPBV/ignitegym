62cfc9dcfebd2dec26f5c5cddb9eebb20dde98f1
Julia Pereira <vital.pereira.julia@gmail.com>
2025-01-31 09:56:45 -0300

    criando rotas do App

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,22 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { Exercise } from "../../src/screens/Exercise";
+import { Home } from "../screens/Home";
+import { History } from "../screens/History";
+import { Profile } from "../screens/Profile";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen name="home" component={Home} />
+
+      <Screen name="history" component={History} />
+
+      <Screen name="profile" component={Profile} />
+
+      <Screen name="exercise" component={Exercise} />
+    </Navigator>
+  );
+}
