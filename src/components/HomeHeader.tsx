2c6a91d69d3cafa86da0d65fe7d1ad402b512ae4
Julia Pereira <vital.pereira.julia@gmail.com>
2025-02-01 11:09:28 -0300

    componente UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,14 @@
 import { Heading, HStack, VStack, Text } from "native-base";
-
+import { UserPhoto } from "./UserPhoto";
 export function HomeHeader() {
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: "https://github.com/juliaPBV.png" }}
+        alt="Imagem do usuário"
+        size={16}
+        mr={4}
+      />
       <VStack>
         <Text color="gray.100" fontSize="md">
           Olá,{" "}
