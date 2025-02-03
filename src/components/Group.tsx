3785f16c844f4e34e424031b8c14084edaae5af7
Julia Pereira <vital.pereira.julia@gmail.com>
2025-02-03 08:32:58 -0300

    estilizando o evento de Pressed

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -14,6 +14,10 @@ export function Group({ name, ...rest }: Props) {
       justifyContent="center"
       alignItems="center"
       overflow="hidden"
+      _pressed={{
+        borderColor: "green.500",
+        borderWidth: 1,
+      }}
       {...rest}
     >
       <Text
