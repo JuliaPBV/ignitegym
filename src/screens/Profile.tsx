4ff32cdab0650a117ed7c15e3b724d91bb308d0b
Julia Pereira <vital.pereira.julia@gmail.com>
2025-02-10 13:25:42 -0300

    botão alterar foto

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,18 +1,45 @@
-import { Center, ScrollView, VStack } from "native-base";
+import { Center, ScrollView, Skeleton, VStack, Text } from "native-base";
 import { ScreenHeader } from "../components/ScreenHeader";
 import { UserPhoto } from "../components/UserPhoto";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{ uri: "https://github.com/JuliaPBV.png" }}
-            alt="Foto do usuário"
-            size={33}
-          />
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: "https://github.com/JuliaPBV.png" }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
         </Center>
       </ScrollView>
     </VStack>
