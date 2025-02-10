ac1fb06566a0bda03f5a85219a99b1d12482e673
Julia Pereira <vital.pereira.julia@gmail.com>
2025-02-10 14:21:39 -0300

    recap da profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -17,10 +17,12 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
-      <ScrollView>
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {photoIsLoading ? (
             <Skeleton
@@ -52,10 +54,14 @@ export function Profile() {
 
           <Input bg="gray.600" placeholder="Nome" />
           <Input bg="gray.600" placeholder="E-mail" isDisabled />
-        </Center>
 
-        <VStack px={10} mt={12} mb={9}>
-          <Heading color="gray.200" fontSize="md" mb={2}>
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            alignSelf="flex-start"
+            mt={12}
+          >
             Alterar senha
           </Heading>
 
@@ -70,7 +76,7 @@ export function Profile() {
           />
 
           <Button title="Atualizar" mt={4} />
-        </VStack>
+        </Center>
       </ScrollView>
     </VStack>
   );
