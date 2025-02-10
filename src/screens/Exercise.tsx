33f9d8a97474af0f6504946008627e4551afccac
Julia Pereira <vital.pereira.julia@gmail.com>
2025-02-10 16:25:31 -0300

    finalizando a interface de exercícios

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,12 @@
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import { Heading, HStack, Icon, VStack, Text, Image } from "native-base";
+import { Heading, HStack, Icon, VStack, Text, Image, Box } from "native-base";
 import { TouchableOpacity } from "react-native";
 import { AppNavigatorRoutesProps } from "../routes/app.routes";
 import BodySvg from "../assets/body.svg";
+import SeriesSvg from "../assets/series.svg";
+import RepetitionsSvg from "../assets/repetitions.svg";
+import { Button } from "../components/Button";
 
 export function Exercise() {
   const navigation = useNavigation<AppNavigatorRoutesProps>();
@@ -50,6 +53,31 @@ export function Exercise() {
           resizeMode="cover"
           rounded="lg"
         />
+
+        <Box bg="gray.600" rounded="md" pb={4} px={4}>
+          <HStack
+            alignItems="center"
+            justifyContent="space-around"
+            mb={6}
+            mt={5}
+          >
+            <HStack>
+              <SeriesSvg />
+              <Text color="gray.200" ml="2">
+                3 séries
+              </Text>
+            </HStack>
+
+            <HStack>
+              <RepetitionsSvg />
+              <Text color="gray.200" ml="2">
+                12 repetições
+              </Text>
+            </HStack>
+          </HStack>
+
+          <Button title="Marcar como realizado" />
+        </Box>
       </VStack>
     </VStack>
   );
